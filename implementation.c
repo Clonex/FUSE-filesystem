020e792df9c64a723d379936ac1ff7d7a0a179c9
Michael <clonex_kontakt@hotmail.com>
2020-05-15 14:06:07 +0200

    Fixed some errors

--- implementation.c
+++ implementation.c
@@ -1,5 +1,5 @@
 entry* findDir(char **pathArr, entry* folder);
-char **splitString(char *input, char delimiter);
+char **splitString(const char *input, char delimiter);
 
 /*
  * Adds a entry to the correct folder.
