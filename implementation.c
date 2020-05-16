b2040b2f6b2dd920f7945766bbae435a4b9359a6
Michael <clonex_kontakt@hotmail.com>
2020-05-16 14:58:34 +0200

    Added debug info

--- implementation.c
+++ implementation.c
@@ -68,7 +68,7 @@ entry* findDir(char **pathArr, entry* folder){
 
     printf("Checking dir..\n");
     entry *currentEntry = folder;
-    for(int pathI = 0; pathI < length; pathI++){
+    for(int pathI = 0; pathI < length - 1; pathI++){
         printf("Current dir: %s\n", currentEntry->name);
         printf("Current path: %s\n", pathArr[pathI]);
 
