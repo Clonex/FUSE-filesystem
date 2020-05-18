0f730a7f0e110d44bfce63ea69b5858e146febb1
Michael <clonex_kontakt@hotmail.com>
2020-05-18 14:16:35 +0200

    Updated saveToDisk

--- disk.c
+++ disk.c
@@ -1,16 +1,53 @@
 int countFolders(entry *file);
+char *createBlock(entry file);
 
 void saveToDisk(entry *file)
 {
     int folders = countFolders(file);
     printf("Total folders: %d\n", folders);
-    /*if(file->type == TYPE_DIR)
+    int length = folders * sizeof(entry);
+    entry *queue = malloc(length);
+    if(queue == NULL)
     {
-        entry *data = (entry *) file->data;
+        return;
+    }
+    char *output = malloc(0);
+    if(output == NULL)
+    {
+        return;
+    }
+    int index = 0;
 
-    }else{
+    memcpy(queue, *file, sizeof(entry));
 
-    }*/
+    while(index < length){
+        entry folder = queue[index];
+        entry *data = (entry *) folder->data;
+        for(int j = 0; j < DEFAULT_DIR_SIZE; j++){
+            entry currentFile = data[j];
+            if(currentFile.type == TYPE_DIR)
+            {
+                memcpy(queue + (index * sizeof(entry)), *file, sizeof(entry));
+                index++;
+            }
+            char *block = createBlock(currentFile);
+            char *tempBlock = malloc((strlen(output) + strlen(block)) * sizeof(char));
+            if(tempBlock == NULL)
+            {
+                return;
+            }
+            memcpy(tempBlock, output, strlen(output) * sizeof(char));
+            strcat(tempBlock, block);
+            free(output);
+            output = tempBlock;
+            printf("Output: %s\n", output);
+        }
+    }
+}
+
+char *createBlock(entry file)
+{
+    return file.name;
 }
 
 int countFolders(entry *file)
@@ -21,7 +58,7 @@ int countFolders(entry *file)
         entry current = data[i];
         if(current.type == TYPE_DIR)
         {
-            ret += countFolders(current);
+            ret += countFolders(&current);
         }
     }
     return ret;
