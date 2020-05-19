d73b28e8fe96e3eb7527e35d528b78f61918f611
kberg18 <kberg18@student.sdu.dk>
2020-05-19 23:20:34 +0200

    trying to fix junk data

--- lfs.c
+++ lfs.c
@@ -11,7 +11,7 @@ int lfs_release(const char *path, struct fuse_file_info *fi);
 int lfs_makefile(const char *path, mode_t mode, dev_t device);
 int lfs_makedir(const char *path, mode_t mode);
 int lfs_write( const char *path, const char *buf, size_t size, off_t offset, struct fuse_file_info *fi );
-int lfs_truncate(const char *path, off_t offset, struct fuse_file_info *fi);
+int lfs_truncate(const char *path, off_t offset);
 int lfs_unlink(const char *path);
 int lfs_rmdir(const char *path);
 static inline int lfs_utime(const char *path, struct utimbuf *buf);
@@ -48,7 +48,7 @@ int lfs_getattr( const char *path, struct stat *stbuf ) {
 	return getAttributes(path, stbuf);
 }
 
-int lfs_truncate(const char *path, off_t offset, struct fuse_file_info *fi) {
+int lfs_truncate(const char *path, off_t offset) {
 	printf("Truncating!\n");
 	entry *file;
 	if((entry *)fi->fh == NULL){
