70fe70ce83ef8aeb8b6e202945bfacee5aced00a
kberg18 <kberg18@student.sdu.dk>
2020-05-19 23:48:20 +0200

    trying to fix junk data

--- lfs.c
+++ lfs.c
@@ -153,6 +153,9 @@ int lfs_write( const char *path, const char *buf, size_t size, off_t offset, str
 		target->data = mem;
 		target->size = offset + tempSize;
 	}
+	printf("Data middle = %s\n", target->data);
+	printf("Size middle  = %d\n", target->size);
+	
 	strcat(target->data, buf);
 	time_t stamp;
     time(&stamp);
