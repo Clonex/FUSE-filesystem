a82bc2ed43be5639abdc6a67e4d2660131bddbb5
kberg18 <kberg18@student.sdu.dk>
2020-05-19 16:05:36 +0200

    added comments in implementation.c

--- implementation.c
+++ implementation.c
@@ -1,14 +1,12 @@
-
-
 /*
- * 
+ * Finds the file associated with the given file-path and saves the attributes of that file into the given struct stat
  */
 int getAttributes(const char *path, struct stat *stbuf){
 	memset(stbuf, 0, sizeof(struct stat));
-	if( strcmp( path, "/" ) == 0 ) {
+	if(strcmp( path, "/" ) == 0){ // root folder
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_nlink = 2;
-	} else {
+	}else{
 		char **tempPath = splitString(path, '/', true);
 		if(tempPath == NULL)
 		{
@@ -33,13 +31,12 @@ int getAttributes(const char *path, struct stat *stbuf){
 		}
 		return -ENOENT;
 	}
-
 	return 0;
 }
 
 
 /*
- * 
+ * Frees all contents of the given directory
  */
 void recursiveRemoveDir(entry* dir)
 {
@@ -56,7 +53,7 @@ void recursiveRemoveDir(entry* dir)
 }
 
 /*
- * Adds a entry to the correct folder.
+ * Creates an entry of type type with the name specified in path. Adds the entry to the parent folder specified in path.
  */
 int createEntry(const char *path, int type)
 {
@@ -65,7 +62,7 @@ int createEntry(const char *path, int type)
         return -ENOMEM;
     }
     
-    if(type == TYPE_DIR)
+    if(type == TYPE_DIR) // directories are filled with blank entries
     {
         file->size = sizeof(entry) * DEFAULT_DIR_SIZE;
         file->data = calloc(DEFAULT_DIR_SIZE, sizeof(entry));
@@ -90,25 +87,25 @@ int createEntry(const char *path, int type)
     file->modTime = stamp;
     file->type = type;
     
-    char **nameArr = splitString(path, '/', true);
-    char **pathArr = splitString(path, '/', false);
+    char **nameArr = splitString(path, '/', true);  // used for retrieving the name of the file
+    char **pathArr = splitString(path, '/', false); // used to find the parent directory
     if(pathArr == NULL || nameArr == NULL)
     {
         return -ENOMEM;
     }
 
     file->name = nameArr[getLength(nameArr) - 1];
-    if(strlen(file->name) > DEFAULT_NAME_SIZE)
+    if(strlen(file->name) > DEFAULT_NAME_SIZE) // filenames caps at 32 chars
     {
         return -ENAMETOOLONG; 
     }
 
     entry *targetDir = root_fs;
-    if(getLength(pathArr) != 1)
+    if(getLength(pathArr) != 1) // if length of pathArr is 1, then the entry must be added to the root directory
     {
         targetDir = findEntry(pathArr);
     }
-    for(int i = 0; i < DEFAULT_DIR_SIZE; i++)
+    for(int i = 0; i < DEFAULT_DIR_SIZE; i++) // find the first blank entry in the parent folder and insert the entry in that spot
     {
         entry *data = (entry *) targetDir->data;
         if(data[i].type == TYPE_BLANK)
@@ -121,22 +118,26 @@ int createEntry(const char *path, int type)
 }
 
 /*
- * Finds the correct entry for a given path.
+ * Returns the entry represented by pathArr. 
  */
-entry* findEntry(char **pathArr){
+entry* findEntry(char **pathArr)
+{
     size_t length = getLength(pathArr);
     entry *currentEntry = root_fs;
-    for(int pathI = 0; pathI < length; pathI++){
-        for(int fileI = 0; fileI < DEFAULT_DIR_SIZE; fileI++){
-            entry *file = (entry *) currentEntry->data;
-            if(file[fileI].type != TYPE_BLANK && strcmp(file[fileI].name, pathArr[pathI]) == 0){
-                currentEntry = &file[fileI];
-                break; // Next path
+    for(int pathI = 0; pathI < length; pathI++) // loop through the steps in the path
+    {
+        for(int fileI = 0; fileI < DEFAULT_DIR_SIZE; fileI++) // loop through the entries in the current directory
+        {
+            entry *data = (entry *) currentEntry->data;
+            if(data[fileI].type != TYPE_BLANK && strcmp(data[fileI].name, pathArr[pathI]) == 0) // the current entry is the current step in the path
+            {
+                currentEntry = &data[fileI];
+                break; // Next step in path
             }
         }
     }
     
-    if(strcmp(currentEntry->name, pathArr[length - 1]) == 0)
+    if(strcmp(currentEntry->name, pathArr[length - 1]) == 0) // the right entry was found
     {
         return currentEntry;
     }
@@ -144,11 +145,11 @@ entry* findEntry(char **pathArr){
 }
 
 /*
- * @returns a array of the input splitted by the delimiter.
+ * Returns an array of the input splitted by the delimiter. includeLast determines whether or not the last entry in the path should be included or not.
  */
 char **splitString(const char *input, char delimiter, bool includeLast){
-    int count = 0;
-    int j = 0;
+    int count = 0;  // number of delimiters in the input
+    int j = 0;      // used to hold position of last delimiter and to find distance between this and the current delimiter
     char **out = calloc(strlen(input), sizeof(char *));
     if(out == NULL)
     {
@@ -178,7 +179,7 @@ char **splitString(const char *input, char delimiter, bool includeLast){
 }
 
 /*
- * 
+ * Returns the length of the given array of strings
  */
 int getLength(char **arr)
 {
